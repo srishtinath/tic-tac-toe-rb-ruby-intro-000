ca8376addf77523f859ed8e097efa0a741428e85
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 02:23:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,7 +42,19 @@ def valid_move?(board, index)
     false
   end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
 
+    if valid_move?(board,index)
+      move(board, index, "X")
+      display_board(board)
+    elsif valid_move?(board,index) == false
+      puts "Please enter valid number."
+      turn(board)
+    end
+end
 
 def won?(board)
   WIN_COMBINATIONS.each { |win_combination|
