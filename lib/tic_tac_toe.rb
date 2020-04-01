c5a7d667cab5c8cf35450462b01547ca2ba80010
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 02:20:27 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,6 +15,25 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(index)
+  index = index.to_i - 1
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
+
 def won?(board)
   WIN_COMBINATIONS.each { |win_combination|
     value_1 = board[win_combination[0]]
