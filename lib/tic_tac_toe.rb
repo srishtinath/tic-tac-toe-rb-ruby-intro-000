b86730b4efdf21823344204606af0042c7a85191
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 02:43:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,8 +18,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(index)
-  index = index.to_i - 1
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
 def move(board, index, character = "X")
